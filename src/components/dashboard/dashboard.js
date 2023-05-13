c1c275324a4a57069a6519d1973a43ad43408393
csworksdev <csworksdev@gmail.com>
2023-05-13 21:24:15 +0700

    change email

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -78,7 +78,7 @@ function DashboardModule() {
   }, [errDeleteActivity, dataDeleteActivity]);
 
   const handleAddActivity = () => {
-    addActivity({ title: "New Activity", email: "ivan@skyshi.com" });
+    addActivity({ title: "New Activity", email: "csworksdev@gmail.com" });
   };
 
   const handleClickDelete = (item) => {
